eb16b5e97c6b4750ea645b8613b9a3fef2626425
Alvaro S <alvaro_s.g@hotmail.com>
2023-06-06 16:02:21 -0300

    Atualização exercícios sql

--- scripts/exercicio.sql
+++ scripts/exercicio.sql
@@ -8,7 +8,7 @@ select nome, qt_vereadores from cidade order by nome asc;
 select nome, qt_vereadores from cidade where qt_vereadores >= 9 order by qt_vereadores ASC;
 
 --Quantidade de cidades que possuem mais de 9 vereadores.
-select nome, qt_vereadores from cidade where qt_vereadores > 9;
+select count(*) from cidade where qt_vereadores > 9;
 
 --Quantidade máxima de vereadores que uma cidade possui.
 select max(qt_vereadores) from cidade; 
@@ -17,14 +17,52 @@ select max(qt_vereadores) from cidade;
 select nome from cidade where qt_vereadores = (select max(qt_vereadores) from cidade);   
 
 --O nome dos candidatos a prefeito ordenados pelo nome.
-select nome, cargo from candidato where cargo = 1 order by nome asc;
+select c.nome, cargo.nome from candidato c
+inner join cargo on cargo.id = c.cargo and cargo.nome = 'Prefeito'
+order by c.nome;
 
 --O nome dos candidatos a vereador que possuem "maria" no nome ordenados pelo nome
-select nome, cargo from candidato where cargo = 2 and nome like '%MARIA%' order by nome asc;
+select c.nome from candidato c 
+inner join cargo on cargo.id = c.cargo  and cargo.nome = 'Vereador'
+where c.nome like '%MARIA %' 
+order by c.nome;
 
 --O nome dos candidatos a vereador que iniciam com 'Y' ordenado pelo nome
-select nome, cargo from candidato where cargo = 2 and nome like 'Y%' order by nome asc;
+select c.nome from candidato c 
+inner join cargo on cargo.id = c.cargo  and cargo.nome = 'Vereador'
+where c.nome like 'Y%' 
+order by c.nome;
 
 --Selecionar o nome dos candidatos a prefeito de cada cidade, ordenado pelo nome da cidade e em seguida pelo nome do candidato
-select car.nome, car.cargo from candidato car where cargo = 1 
-inner join cidade cid  
+select cidade.nome Cidade, candidato.nome Candidato from cidade
+inner join candidato on candidato.cidade = cidade.id 
+inner join cargo on cargo.id = candidato.cargo and cargo.nome = 'Prefeito'
+order by cidade.nome, candidato.nome;
+
+--Selecionar o nome dos candidatos a prefeito da cidade 'TUBARÃO', ordenado pelo nome do candidato.
+select cidade.nome Cidade, candidato.nome Candidato from cidade
+inner join candidato on candidato.cidade = cidade.id and cidade.nome = 'TUBARÃO'
+inner join cargo on cargo.id = candidato.cargo and cargo.nome = 'Prefeito'
+order by cidade.nome, candidato.nome;
+
+--Selecionar o nome dos candidatos a prefeito da cidade que tem o maior número de eleitores.
+select candidato.nome Candidato, cidade.nome Cidade, cidade.qt_eleitores Eleitores from cidade
+inner join candidato on candidato.cidade = cidade.id
+inner join cargo on cargo.nome = 'Prefeito'
+order by cidade.qt_eleitores desc;
+
+--Selecionar a quantidade de candidatos a vereador da cidade de 'TUBARÃO'.
+select count(*) TotalVereadores from (
+  select cidade.nome Cidade, candidato.nome Candidato, count(*) as Total from cidade
+  inner join candidato on candidato.cidade = cidade.id and cidade.nome = 'TUBARÃO'
+  inner join cargo on cargo.id = candidato.cargo and cargo.nome = 'Vereador'
+  group by cidade.nome, candidato.nome
+) as subquery;
+
+--Selecionar a quantidade de candidatos a vereador de cada cidade, ordenado pelo nome da cidade.
+select count (*) TotalVereadores, cidade Cidade from (
+  select cidade.nome Cidade, candidato.nome Candidato, count(*) as Total from cidade
+  inner join candidato on candidato.cidade = cidade.id
+  inner join cargo on cargo.id = candidato.cargo and cargo.nome = 'Vereador'
+  group by cidade.nome, candidato.nome
+) as subquery;
